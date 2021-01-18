6ce122e2d29f6d6aec7a9fe9b5bc5d3e900000d7
Jacques Blom <39128232+jacques-blom@users.noreply.github.com>
2021-01-18 23:16:28 +0200

    Remove buttons

--- src/Toolbar.tsx
+++ src/Toolbar.tsx
@@ -1,6 +1,5 @@
 import {Icon, IconButton, VStack} from '@chakra-ui/react'
-import {Folder, GitHub, Save, Square} from 'react-feather'
-import {filePersist} from './FilePersist'
+import {Square} from 'react-feather'
 import {useAddElement} from './state'
 
 export const Toolbar = () => {
@@ -21,22 +20,6 @@ export const Toolbar = () => {
                 onClick={() => addElement()}
                 aria-label="Add rectangle"
                 icon={<Icon style={{width: 24, height: 24}} as={Square} />}
-            />
-            <IconButton
-                onClick={() => filePersist.openFile()}
-                aria-label="Open drawing"
-                icon={<Icon style={{width: 24, height: 24}} as={Folder} />}
-            />
-            <IconButton
-                onClick={() => filePersist.saveFile()}
-                aria-label="Save drawing"
-                icon={<Icon style={{width: 24, height: 24}} as={Save} />}
-            />
-            <IconButton
-                as="a"
-                aria-label="View on GitHub"
-                icon={<Icon style={{width: 24, height: 24}} as={GitHub} />}
-                href="https://github.com/jacques-blom/recoil-course"
             />
         </VStack>
     )
