07e0f3f6cfd413a7b1a646213d59490de3b9d82a
Jacques Blom <39128232+jacques-blom@users.noreply.github.com>
2021-01-26 08:44:30 +0200

    Add Recoil package and RecoilRoot

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,21 @@ import './index.css'
 import Canvas from './Canvas'
 import {ChakraProvider} from '@chakra-ui/react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {RecoilRoot} from 'recoil'
 
 ReactDOM.render(
     <React.StrictMode>
-        <ChakraProvider>
-            <Router>
-                <Switch>
-                    <Route>
-                        <Canvas />
-                    </Route>
-                </Switch>
-            </Router>
-        </ChakraProvider>
+        <RecoilRoot>
+            <ChakraProvider>
+                <Router>
+                    <Switch>
+                        <Route>
+                            <Canvas />
+                        </Route>
+                    </Switch>
+                </Router>
+            </ChakraProvider>
+        </RecoilRoot>
     </React.StrictMode>,
     document.getElementById('root'),
 )
